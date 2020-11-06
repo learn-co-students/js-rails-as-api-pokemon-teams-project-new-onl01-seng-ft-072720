f704bc262d93d32c7caf5e11fb50f746383ec148
aleebrady <aleebrady@gmail.com>
2020-11-05 20:58:59 -0800

    Done.

---
+++ pokemon-teams-backend/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # resources :pokemons
+  # resources :trainers
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/trainers' => 'trainers#index'
+  post '/pokemons' => 'pokemons#create'
+  delete '/pokemons/:id' => 'pokemons#destroy'
+end
