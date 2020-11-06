892831426f42bab0eb5c2b0b42ec17f6cc491fed
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-06 11:58:38 -0500

    Done.

--- pokemon-teams-backend/config/routes.rb
+++ pokemon-teams-backend/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :pokemons
-  resources :trainers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pokemons, only: [:index, :show, :create, :destroy]
+  resources :trainers, only: [:index, :show]
 end
